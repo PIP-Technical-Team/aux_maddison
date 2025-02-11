11710820bac85849c6c22cb0c47a0041227ebfd0
R.Andres Castaneda Aguilar <acastanedaa@worldbank.org>
2025-02-11 17:50:32 -0500

    update maddison using 2023 database rather than 2020

--- R/download_maddison.R
+++ R/download_maddison.R
@@ -1,19 +1,24 @@
 library(data.table)
-path <- "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta"
-mpd <- haven::read_dta(path)
-mpd <- mpd[mpd$year >= 1960, ] # Historical data is not needed
 
-setDT(mpd)
+# path1 <- "https://www.rug.nl/ggdc/historicaldevelopment/maddison/data/mpd2020.dta"
+# mpd1 <- haven::read_dta(path1)
+# setDT(mpd1)
+# mpd1 <- mpd1[year >= 1960] # Historical data is not needed
+
+path2 <- "https://dataverse.nl/api/access/datafile/421303"
+mpd2 <- haven::read_dta(path2)
+setDT(mpd2)
+mpd2 <- mpd2[year >= 1960] # Historical data is not needed
+
+# waldo::compare(mpd1[countrycode == "COL" & year < 2019, .(year, gdppc)],
+#                mpd2[countrycode == "COL" & year < 2019, .(year, gdppc)])
 
 # Recode names
-setnames(mpd,
+setnames(mpd2,
          old = c("countrycode", "country", "gdppc"),
          new = c("country_code", "country_name", "mpd_gdp")
 )
 # Keep relevant variables
-mpd <- mpd[
-  ,
-  .(country_code, year, mpd_gdp)
-]
+mpd <- mpd2[,.(country_code, year, mpd_gdp)]
 
 readr::write_csv(mpd, "maddison.csv")
